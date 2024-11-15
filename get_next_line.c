8f32cc3a91b90b8c1bbe06eb2b69cea97b20a09f
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-15 23:46:52 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -85,8 +85,11 @@ char	*get_next_line(int fd)
 		if (!saved || !ft_strchr(saved, '\n'))
     	if (!read_line(fd, buff, &saved))
 			{
-        line = extract_line(saved);
-				saved = update_saved(saved, ft_strlen(line));
+				if (saved)
+				{
+					line = extract_line(saved);
+					saved = update_saved(saved, ft_strlen(line));
+				}
 			}
 		if (!saved)
 			free(saved);
