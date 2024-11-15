1a02f7e9c0a98edfc8d5c21ea49adbbf51858234
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-15 18:10:21 +0300

    chtagpt

--- get_next_line.c
+++ get_next_line.c
@@ -1,103 +1,70 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ataan <ataan@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/08 19:28:19 by ataan             #+#    #+#             */
-/*   Updated: 2024/11/14 20:15:58 by ataan            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
-static void	read_line(int fd, char **draft, char **buff)
+static void	read_line(int fd, char **draft, char *buff)
 {
-	int		bytes_read;
-	char	*tmp;
+    int		bytes_read;
+    char	*tmp;
 
-	bytes_read = 1;
-	while (bytes_read > 0)
-	{
-		bytes_read = read(fd, buff, BUFFER_SIZE);
-		if (bytes_read <= 0)
-			return (free(buff));
-		buff[bytes_read] = '\0';
-		tmp = *draft;
-		if (!*draft) // first try not used before, == NULL
-			*draft = ft_strdup(*buff);
-		else
-			*draft = ft_strjoin(*draft, *buff);
-		free(tmp);
-		if (ft_strchr(*draft, '\n'))
-			break ;
-	}
-	free(buff);
+    bytes_read = 1;
+    while (bytes_read > 0)
+    {
+        bytes_read = read(fd, buff, BUFFER_SIZE);
+        if (bytes_read <= 0)
+            return; // No more data, or error
+        buff[bytes_read] = '\0';
+        tmp = *draft;
+        if (!*draft)
+            *draft = ft_strdup(buff);
+        else
+            *draft = ft_strjoin(*draft, buff);
+        free(tmp);
+        if (ft_strchr(*draft, '\n'))
+            break;
+    }
 }
 
 static char	*extract_line(char *draft)
 {
-  int i;
+    int i = 0;
 
-  i = 0;
-  if (ft_strchr(draft, '\n'))
-  {
-	while (draft[i] != '\n')
-  		i++;
-	return(ft_substr(draft, 0, i + 1));
-  }
-  return (ft_strdup(draft));
+    if (ft_strchr(draft, '\n'))
+    {
+        while (draft[i] != '\n')
+            i++;
+        return (ft_substr(draft, 0, i + 1)); // Extract line including newline
+    }
+    return (ft_strdup(draft)); // Return entire draft if no newline
 }
-
-/* static char	*extract_line(int fd, char *drafts)
-// {
-// 	char	*line;
-// 	char	*tmp;
-// 	int		i;
-
-// 	i = 0;
-// 	if (ft_strchr(drafts[fd], '\n')) // new line exists in drafts
-// 	{
-// 		while (drafts[fd][i] != '\n')
-// 			i++;
-// 		line = ft_substr(drafts[fd], 0, i + 1); // this usese malloc
-// 		tmp = drafts[fd];
-// 		drafts[fd] = ft_substr(drafts[fd], i + 1, (ft_strlen(drafts[fd])
-// 					- ft_strlen(line))); // this uses malloc
-// 		free(tmp);
-// 	}
-// 	else // nl not found in drafts
-// 	{
-// 		line = ft_strdup(drafts[fd]);
-// 		free(drafts[fd]);
-// 		drafts[fd] = NULL;
-// 	}
-// 	return (line);
-// }*/
 
 char	*get_next_line(int fd)
 {
-	static char	*drafts[FOPEN_MAX];
-	char *line;
-	char *tmp;
-	char *buff;
+    static char	*drafts[FOPEN_MAX];
+    char *line;
+    char *tmp;
+    char *buff;
 
-	if (BUFFER_SIZE <= 0 || fd < 0 || fd >= FOPEN_MAX)
-		return (NULL);
-	buff = (char *)malloc(BUFFER_SIZE + 1);
-	if (!buff)
-			return NULL;
-	read_line(fd, &drafts[fd], &buff);
-	if (!drafts[fd] || drafts[fd][0] == '\0')
-		return (NULL); // No more data to read
-	line = extract_line(drafts[fd]);
-	tmp = drafts[fd];
-	drafts[fd] = ft_substr(drafts[fd], ft_strlen(line), (ft_strlen(drafts[fd])
-						- ft_strlen(line)));
-	free(tmp);
-	return (line);
+    if (BUFFER_SIZE <= 0 || fd < 0 || fd >= FOPEN_MAX)
+        return (NULL); // Invalid buffer size or file descriptor
+
+    buff = (char *)malloc(BUFFER_SIZE + 1);
+    if (!buff)
+        return (NULL); // Memory allocation failure
+
+    read_line(fd, &drafts[fd], buff);
+    if (!drafts[fd] || drafts[fd][0] == '\0')
+    {
+        free(buff);
+        return (NULL); // No data or empty file
+    }
+
+    line = extract_line(drafts[fd]);
+    tmp = drafts[fd];
+    size_t remaining_len = ft_strlen(drafts[fd]) - ft_strlen(line);
+    drafts[fd] = ft_substr(drafts[fd], ft_strlen(line), remaining_len);
+    free(tmp);
+    free(buff);
+    return (line);
 }
