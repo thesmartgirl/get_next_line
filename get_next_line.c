a38faadabeafc816c5ac5be3e4365539f0fb2ebd
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-15 20:22:39 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -66,10 +66,10 @@ char *get_next_line(int fd)
         buff[bytes_read] = '\0'; // Null-terminate the buffer
         char *temp = saved_data;
         saved_data = ft_strjoin(saved_data, buff); // Append new data to saved_data
-        free(temp);
+        // free(temp);
         if (!saved_data)
         {
-            // free(buff);
+            free(buff);
             return NULL; // Memory allocation failure
         }
     }
