d9353ea7ddba68713322efa329df7db04d26a28f
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-15 20:16:24 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -51,8 +51,8 @@ char *get_next_line(int fd)
     if (!buff || fd < 0)
         return NULL;
 
+				while (!saved_data || !ft_strchr(saved_data, '\n'))
     // Read more data into saved_data if necessary
-    while (!saved_data || !strchr(saved_data, '\n'))
     {
         ssize_t bytes_read = read(fd, buff, BUFFER_SIZE);
         if (bytes_read <= 0) // No more data to read
