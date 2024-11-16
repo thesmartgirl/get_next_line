eff0d5b259fc97d72fa8d990e8a1f36ed9759275
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-16 19:52:01 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -102,13 +102,13 @@ char	*get_next_line(int fd)
 						// free(temp);
 					}
 		}
-		else
-    {
-        free(saved);  // In case of read error, free saved.
-        saved = NULL;  // Set saved to NULL to avoid dangling pointer.
-    }
+		// else
+    // {
+    //     free(saved);  // In case of read error, free saved.
+    //     saved = NULL;  // Set saved to NULL to avoid dangling pointer.
+    // }
 		if (line == NULL) {
 			free(saved);
 		}
-		 return line;
+		return line;
 }
