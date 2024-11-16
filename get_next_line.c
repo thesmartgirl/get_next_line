7b99bf97e8ac175b7e9cb334f868755866391ddf
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-16 14:15:40 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -15,6 +15,8 @@ static int read_line(int fd, char **saved)
 		return 1;
 	}
 	bytes_read = 1;
+	if (!saved)
+		saved = ft_strdup("");
 	while (bytes_read > 0 && !ft_strchr(*saved, '\n'))
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
