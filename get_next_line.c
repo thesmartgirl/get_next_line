dd1dd40bfe8a1186e06c045a33cc2af5d0a29dd4
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-18 18:26:15 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -61,37 +61,6 @@ static char *update_line_draft(char **line_draft, int fd, char *line)
 	return line_draft[fd];
 }
 
-// static char	*read_line(char *buff, char **line_draft, int fd)
-// {
-// 	int	bytes_read;
-//
-// 	bytes_read = 1;
-// 	while (bytes_read > 0)
-// 	{
-// 		if (!ft_strchr(line_draft[fd], '\n'))
-// 		{
-// 			buff = (char *)malloc(BUFFER_SIZE + 1);
-// 			if (buff == NULL)
-// 			{
-// 				cleanup_fd(line_draft, fd);
-// 				return (NULL);
-// 			}
-// 			bytes_read = read(fd, buff, BUFFER_SIZE);
-// 			if (bytes_read == -1)
-// 			{
-// 				free(buff);
-// 				cleanup_fd(line_draft, fd);
-// 				return (NULL);
-// 			}
-// 			buff[bytes_read] = '\0';
-// 			line_draft[fd] = ft_strjoin(line_draft[fd], buff);
-// 			free(buff);
-// 		}
-// 		else
-// 			return ("OK");
-// 	}
-// 	return ("OK"); //EOF
-// }
 static char	*read_line(char *buff, char **line_draft, int fd)
 {
 	int	bytes_read;
