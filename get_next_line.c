8095faae1d11a0995f0fb761ea0fc17be894f43b
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-18 14:24:33 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 static char	*extract_line(char **line_draft, int fd)
 {
