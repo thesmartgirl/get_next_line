e94f28fd9575dae625792ad0d4aa724b2ce41979
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-11-18 14:23:50 +0300

    .

--- get_next_line.h
+++ get_next_line.h
@@ -1,23 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ataan <ataan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/08 19:28:52 by ataan             #+#    #+#             */
-/*   Updated: 2024/11/14 15:01:29 by ataan            ###   ########.fr       */
+/*   Created: 2024/11/08 19:27:49 by ataan             #+#    #+#             */
+/*   Updated: 2024/11/12 17:50:13 by ataan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
+# ifndef FD_MAX
+#  define FD_MAX FOPEN_MAX
+# endif
+
+# if FD_MAX > FOPEN_MAX
+#  undef FD_MAX
+#  define FD_MAX FOPEN_MAX
+# endif
+
+# if FD_MAX < 1
+#  undef FD_MAX
+#  define FD_MAX FOPEN_MAX
+# endif
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE BUFSIZ
+# endif
+
+# if BUFFER_SIZE > 1000000
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
 # endif
 
 # if BUFFER_SIZE < 1
@@ -26,10 +45,10 @@
 # endif
 
 char			*get_next_line(int fd);
-size_t	        ft_strlen(const char *str);
-char	    *ft_strchr(const char *s, int c);
-char	    *ft_strdup(const char *str);
-char	    *gnl_strjoin(char *s1, char const *s2);
-char	    *ft_substr(char const *s, unsigned int start, size_t len);
+int				ft_strlen(char *str);
+int				ft_strchr(char *str, char ch);
+char			*ft_strdup(char *original);
+char			*ft_strjoin(char *s1, char *s2);
+char			*ft_substr(char *str, int start, int max);
 
 #endif
